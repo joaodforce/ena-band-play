c82a57d91c33e7a91a30fd51c65c0e42461a5cd5
João Eduardo Soares e Silva <jed@Joaos-MacBook-Air.local>
2023-10-29 09:52:35 -0300

    Added support for title and thubnail on media foundation and added a button to start the stream on the main page

--- Ena Band Play/ViewController.swift
+++ Ena Band Play/ViewController.swift
@@ -7,14 +7,33 @@
 
 import UIKit
 import AVKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
 
     private lazy var jsonDecoder = JSONDecoder()
     private var urlSession: URLSession?
+    private var activityIndicator: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupActivityIndicator()
+        // Do any additional setup after loading the view.
+    }
+    
+    // Function to set up the activity indicator
+        func setupActivityIndicator() {
+            activityIndicator = UIActivityIndicatorView(style: .large)
+            activityIndicator.center = view.center
+            activityIndicator.hidesWhenStopped = true
+            view.addSubview(activityIndicator)
+        }
+    
+    @IBAction func abrirBand(_ sender: Any) {
+        
+        
+        activityIndicator.startAnimating()
         
         urlSession = URLSession(configuration: URLSessionConfiguration.default)
         
@@ -26,6 +45,11 @@ class ViewController: UIViewController {
 //        request.addValue(jwtToken, forHTTPHeaderField: "Authorization")
         
         let task = self.urlSession!.dataTask(with: request) { (data, response, error) in
+            
+            // Dismiss the activity indicator on the main queue
+            DispatchQueue.main.async {
+                self.activityIndicator.stopAnimating()
+            }
             
             if let httpResponse = response as? HTTPURLResponse {
                 let statusCode = httpResponse.statusCode
@@ -43,47 +67,99 @@ class ViewController: UIViewController {
                         }
                         
                         struct DecoderedThing: Decodable {
-                            var qualities: StreamQualities
+                            var qualities: StreamQualities?
                         }
+                        
+//                        let jsonStr = String(data: _data, encoding: .utf8)
                         
                         let objs = try self.jsonDecoder.decode(DecoderedThing.self, from: _data)
                         
                         DispatchQueue.main.async {
-                            self.openStreame(stream: objs.qualities.auto.first!.url)
+                            if let qualities = objs.qualities {
+                                self.openStream(stream: qualities.auto.first!.url)
+                            } else {
+                                let alert = UIAlertController(title: "API did not give any Streaming URL SMH", message: nil, preferredStyle: .alert)
+                                self.present(alert, animated: true)
+                            }
                         }
                         
                         
                     } else {
-                        let alert = UIAlertController(title: "Falha ao carregar Stream", message: error?.localizedDescription, preferredStyle: .alert)
-                        self.present(alert, animated: true)
+                        DispatchQueue.main.async {
+                            let alert = UIAlertController(title: "Falha ao carregar Stream", message: error?.localizedDescription, preferredStyle: .alert)
+                            self.present(alert, animated: true)
+                        }
                     }
                 } catch {
-                    let alert = UIAlertController(title: "Falha Ao carregar URL", message: error.localizedDescription, preferredStyle: .alert)
-                    self.present(alert, animated: true)
+                    DispatchQueue.main.async {
+                        let alert = UIAlertController(title: "Falha Ao carregar URL", message: error.localizedDescription, preferredStyle: .alert)
+                        self.present(alert, animated: true)
+                    }
                 }
             }
         }
         task.resume()
         
-        
-        
-        // Do any additional setup after loading the view.
     }
     
-    func openStreame(stream: URL) {
+    func openStream(stream: URL) {
+
+        // Create an AVPlayerItem
+        let playerItem = AVPlayerItem(url: stream)
+
+        // Create metadata items for title, description, and thumbnail
+        let titleItem = AVMutableMetadataItem()
+        titleItem.identifier = AVMetadataIdentifier.commonIdentifierTitle
+        titleItem.value = "Band Ao Vivo" as NSString
+
+//        let descriptionItem = AVMutableMetadataItem()
+//        descriptionItem.identifier = AVMetadataIdentifier.commonIdentifierDescription
+//        descriptionItem.value = "" as NSString
         
-        let playerVC = self.storyboard?.instantiateViewController(withIdentifier: "playerVC") as? AVPlayerViewController
+        // Create a metadata item for the thumbnail (artwork)
+        let thumbnailImage = UIImage(named: "band-play")
+        let thumbnailData = thumbnailImage?.pngData() as NSData?
+        let thumbnailItem = AVMutableMetadataItem()
+        thumbnailItem.identifier = AVMetadataIdentifier.commonIdentifierArtwork
+        thumbnailItem.value = thumbnailData
         
-        let pl = AVPlayer(url: stream)
         
-//        pl.meta
+        // Add the metadata items to the player item's metadata
+        playerItem.externalMetadata = [titleItem, thumbnailItem]
+
+        // Create an AVPlayer with the player item
+        let player = AVPlayer(playerItem: playerItem)
+
+        // Create an AVPlayerViewController and set the player
+        let playerViewController = AVPlayerViewController()
+        playerViewController.player = player
         
-        playerVC?.player = pl
+//        player.seek = false
         
-        self.present(playerVC!, animated: true)
-        
-        pl.play()
+        // Present the player view controller
+        present(playerViewController, animated: true) {
+            player.play()
+        }
+
     }
+    
+//    func openStreame(stream: URL) {
+//        
+//        
+//        let playerVC = self.storyboard?.instantiateViewController(withIdentifier: "playerVC") as? AVPlayerViewController
+//        
+//        let pl = AVPlayer(url: stream)
+//        
+////        pl.meta
+//        // Indicate that the stream is not seekable
+//        pl.currentItem?.isSeekable = false
+//
+//        playerVC?.player = pl
+//        
+//        self.present(playerVC!, animated: true)
+//        
+//        pl.play()
+//    }
 
 
 }
---
+++ Ena Play/Ena_PlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ena_PlayApp.swift
+//  Ena Play
+//
+//  Created by João Eduardo on 02/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Ena_PlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
